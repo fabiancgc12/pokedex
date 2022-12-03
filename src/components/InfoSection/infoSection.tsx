056788281951479ab7cb301c6b79ade1db1d0c85
fabian graterol <fabiancgraterol@gmail.com>
2022-12-02 20:43:04 -0400

    creating info section -back side

--- src/components/InfoSection/infoSection.tsx
+++ src/components/InfoSection/infoSection.tsx
@@ -13,8 +13,8 @@ export function InfoSection(){
 
     return (
         <div className={styles.wrapper}>
-            <div className={styles.front}>
-                <TopSeparator className={styles.separator}/>
+            <div className={styles.frontside}>
+                <TopSeparator/>
                 <button className={styles.backButton} onClick={clickOnBack}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          className="bi bi-caret-left-fill" viewBox="0 0 16 16">
@@ -24,7 +24,19 @@ export function InfoSection(){
                     Back
                 </button>
             </div>
-
+            <div className={styles.backside}>
+                <TopSeparator className={styles.separator}/>
+                <div className={styles.case}>
+                    <div className={styles.icon}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                            <path
+                                d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+                        </svg>
+                    </div>
+                    <span>OPEN</span>
+                </div>
+            </div>
         </div>
     )
 }
